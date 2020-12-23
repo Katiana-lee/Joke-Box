410a8331a4ee98febf6da8f60597e69c809760b2
Katiana <katiana002@gmail.com>
2020-12-23 12:32:11 -0500

    API call

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('ready to start')
+let getData = async () => {
+  try {
+    const response = await axios.get(`https://cors-anywhere.herokuapp.com/https://official-joke-api.appspot.com/jokes/programming/random`)
+    console.log(response.data)
+  } catch (error) {
+    console.log(error)
+  }
+ }
+ getData()
