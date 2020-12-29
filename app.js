6cb47212b0e3ccc9b2480095b081e3d8e4b4404c
Katiana <katiana002@gmail.com>
2020-12-28 20:32:09 -0500

    button are working

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+let programSetup = document.querySelector('.program-setup')
+let programPunchLine = document.querySelector('.program-punchline')
+let jokeType = document.querySelector('.joke-type')
+
 let getProgramJoke = async () => {
   try {
     const response = await axios.get(`https://official-joke-api.appspot.com/jokes/programming/random`)
-    console.log(response.data)
+    // console.log(response.data)
+    jokeType.textContent = "Programmer"
+    programSetup.textContent = response.data[0].setup
+    programPunchLine.textContent = response.data[0].punchline
   } catch (error){
     console.log(error)
   }
@@ -9,7 +16,10 @@ let getProgramJoke = async () => {
 let getRandomJoke = async () => {
   try {
     const response = await axios.get(`https://official-joke-api.appspot.com/jokes/random`)
-    console.log(response.data)
+    // console.log(response.data)
+    jokeType.textContent = "Random"
+    programSetup.textContent = response.data.setup
+    programPunchLine.textContent = response.data.punchline
   } catch (error){
     console.log(error)
   }
@@ -18,6 +28,9 @@ let getKnockJoke = async () => {
   try {
     const response = await axios.get(`https://official-joke-api.appspot.com/jokes/knock-knock/random`)
     console.log(response.data)
+    jokeType.textContent = "Knock-Knock"
+    programSetup.textContent = response.data[0].setup
+    programPunchLine.textContent = response.data[0].punchline
   } catch (error){
     console.log(error)
   }
@@ -27,7 +40,8 @@ let getKnockJoke = async () => {
  
 let programmerBtn = document.querySelector('.programmer')
 programmerBtn.addEventListener('click', getProgramJoke)
-let randomBtn = document.querySelector('.general')
+let randomBtn = document.querySelector('.random')
 randomBtn.addEventListener('click', getRandomJoke)
 let knockBtn = document.querySelector('.knock-knock')
 knockBtn.addEventListener('click', getKnockJoke)
+
