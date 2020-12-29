1fc5601cee181a25063264ca34f884f1f6dbb40a
Katiana <katiana002@gmail.com>
2020-12-28 21:14:59 -0500

    added giphy to programmer joke

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 let programSetup = document.querySelector('.program-setup')
 let programPunchLine = document.querySelector('.program-punchline')
 let jokeType = document.querySelector('.joke-type')
+let giphy = document.querySelector('.joke-giphy')
+let counter = 0
 
 let getProgramJoke = async () => {
   try {
     const response = await axios.get(`https://official-joke-api.appspot.com/jokes/programming/random`)
     // console.log(response.data)
     jokeType.textContent = "Programmer"
+    // giphy.src = `https://api.giphy.com/v1/gifs/search?api_key=6hcoep97hU9v6cEou7WlVp5UNs3OwIuW&q=laughing&limit=1&offset=${counter}&rating=g&lang=en`
     programSetup.textContent = response.data[0].setup
     programPunchLine.textContent = response.data[0].punchline
   } catch (error){
@@ -35,11 +38,22 @@ let getKnockJoke = async () => {
     console.log(error)
   }
  }
-
+ let getProgramGiphy = async () => {
+  try {
+    const response = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=6hcoep97hU9v6cEou7WlVp5UNs3OwIuW&q=laughing&limit=1&offset=${counter}&rating=g&lang=en`)
+    jokeType.textContent = "Programmer"
+    giphy.src = response.data.data[0].images.original.url
+    // console.log(response.data)
+  } catch (error){
+    console.log(error)
+  }
+}
 // getData()
  
 let programmerBtn = document.querySelector('.programmer')
 programmerBtn.addEventListener('click', getProgramJoke)
+programmerBtn.addEventListener('click', getProgramGiphy)
+
 let randomBtn = document.querySelector('.random')
 randomBtn.addEventListener('click', getRandomJoke)
 let knockBtn = document.querySelector('.knock-knock')
