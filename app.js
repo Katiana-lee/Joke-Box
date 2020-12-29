12b097c6ad5022d880ea297acefe9f3ac34f65ca
Katiana <katiana002@gmail.com>
2020-12-28 19:58:30 -0500

    buttons are working

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
-let getData = async () => {
+let getProgramJoke = async () => {
   try {
-    const response = await axios.get(`https://cors-anywhere.herokuapp.com/https://official-joke-api.appspot.com/jokes/programming/random`)
+    const response = await axios.get(`https://official-joke-api.appspot.com/jokes/programming/random`)
+    console.log(response.data)
+  } catch (error){
+    console.log(error)
+  }
+}
+let getRandomJoke = async () => {
+  try {
+    const response = await axios.get(`https://official-joke-api.appspot.com/jokes/random`)
+    console.log(response.data)
+  } catch (error){
+    console.log(error)
+  }
+}
+let getKnockJoke = async () => {
+  try {
+    const response = await axios.get(`https://official-joke-api.appspot.com/jokes/knock-knock/random`)
     console.log(response.data)
   } catch (error){
     console.log(error)
   }
  }
-getData()
+
+// getData()
  
 let programmerBtn = document.querySelector('.programmer')
-programmerBtn.addEventListener('click', () => {
-  console.log('you clicked me')
-})
+programmerBtn.addEventListener('click', getProgramJoke)
+let randomBtn = document.querySelector('.general')
+randomBtn.addEventListener('click', getRandomJoke)
+let knockBtn = document.querySelector('.knock-knock')
+knockBtn.addEventListener('click', getKnockJoke)
