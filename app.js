8ce3f9554874a46542c5fcaa300c8ca37cee854f
Katiana <katiana002@gmail.com>
2020-12-28 22:03:25 -0500

    random giphys are working properly

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ let getProgramJoke = async () => {
     // console.log(response.data)
     jokeType.textContent = "Programmer"
     // giphy.src = `https://api.giphy.com/v1/gifs/search?api_key=6hcoep97hU9v6cEou7WlVp5UNs3OwIuW&q=laughing&limit=1&offset=${counter}&rating=g&lang=en`
-    programSetup.textContent = response.data[0].setup
+    programSetup.textContent = response.data[0].setup //data is showing as an array first thats why the bracket notation is used...array then object
     programPunchLine.textContent = response.data[0].punchline
   } catch (error){
     console.log(error)
@@ -20,7 +20,7 @@ let getRandomJoke = async () => {
     const response = await axios.get(`https://official-joke-api.appspot.com/jokes/random`)
     // console.log(response.data)
     jokeType.textContent = "Random"
-    programSetup.textContent = response.data.setup
+    programSetup.textContent = response.data.setup //data is an object so bracket was not needed..object by itself
     programPunchLine.textContent = response.data.punchline
   } catch (error){
     console.log(error)
@@ -41,9 +41,8 @@ let getKnockJoke = async () => {
    try {
 let counter = Math.floor(Math.random() * 1000)
     const response = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=6hcoep97hU9v6cEou7WlVp5UNs3OwIuW&q=laughing&limit=1&offset=${counter}&rating=g&lang=en`)
-    jokeType.textContent = "Programmer"
-    giphy.src = response.data.data[0].images.original.url
-    console.log(response.data)
+    giphy.src = response.data.data[0].images.original.url 
+    // console.log(response.data)
   } catch (error){
     console.log(error)
   }
